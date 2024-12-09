95f3487659f079a1a737fd2dc8e61d64c6f4fe7c
AungKaungMyat5069 <aungkaungmyat5069@gmail.com>
2024-12-10 00:19:15 +0630

    Skills, Services, Work, Tesimonials and Contact is added

--- script.js
+++ script.js
@@ -6,3 +6,49 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+/* *************** SERVICES MODAL *********************** */
+const modalViews = document.querySelectorAll(".services__modal"),
+    modalBtns = document.querySelectorAll(".services__button"),
+    modalClose = document.querySelectorAll(".services__modal-close")
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener("click", () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach(mv => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+/* ****************** SWIPER TESTIMONIAL ************** */
+var swiper = new Swiper(".testimonial__container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      960: {
+        slidesPerView: 2,
+        spaceBetween: 48,
+      },
+      
+    },
+  });
